ae2e95fc1cb111d69fc4b56d8ade7d1b06972546
Alicja Wilk <alicja.wilk96@gmail.com>
2019-10-12 00:56:07 +0200

    adding Menu component

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,11 @@ import './styles/styles.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Main from './pages/Main';
+import Menu from './components/Menu';
 import PrivateRoute from './PrivateRoute';
 import { AuthContext } from './context/';
 import { ROUTES, STORAGE_NAMES } from './constants';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const sessionStorageToken = sessionStorage.getItem(STORAGE_NAMES.TOKEN);
@@ -21,6 +23,7 @@ const App = () => {
   return (
       <AuthContext.Provider value={{authTokens, setAuthTokens: setTokens}}>
         <Router>
+          <Menu />
               <Route exact path={ROUTES.HOME} component={Home}/>
               <PrivateRoute path={ROUTES.MAIN} component={Main}/>
         </Router>
