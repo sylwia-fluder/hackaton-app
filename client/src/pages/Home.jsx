c342f842c28076c3dd757fbafed5889ef099c116
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2019-10-12 08:06:38 +0200

    clear code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Main from '../components/UsersList/UserList';
+
 
 const Home = () => {
-    const userList= [{name:'Adrian',name:'Podkalicki',done:true},{username:'MArcin',name:'Kowalski',done:false}]
+    
     return (
         <div>
             Home Page
-           <Main usersList = {userList}/>
+          
         </div>
     );
 };
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Main from '../components/UsersList/UserList';
 
 const Project = () => {
+    const userList= [{name:'Adrian',name:'Podkalicki',done:true},{username:'MArcin',name:'Kowalski',done:false}]
     return (
         <div>
             Single Project
+             <Main usersList = {userList}/>
         </div>
     );
 };
