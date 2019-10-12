3c0bdf078b38af9546077fb7d8321579be6c8b42
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2019-10-12 06:33:06 +0200

    function createIcon, createDynamicClass

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Main from '../components/UserMetadataTile/UserMetadataTile'
 const Home = () => {
     return (
         <div>
             Home Page
+            <Main done = {false} name ={'siema'} surname = {'siema'}/>
         </div>
     );
 };
