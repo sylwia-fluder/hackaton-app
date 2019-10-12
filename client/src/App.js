2b59adf5c50f9090800b154a670784f6a16d70e2
Sylwia <sylwia.fluder@applover.pl>
2019-10-12 09:47:01 +0200

    Fetch projects

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,39 @@
-import React, { useState } from 'react';
-import './styles/styles.scss';
-import { Router, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Main from './pages/Main';
-import Project from './pages/Project';
-import FeedbackForm from './pages/FeedbackForm';
-import UserProfile from './pages/UserProfile';
-import Menu from './components/Menu';
-import PrivateRoute from './PrivateRoute';
-import history from './history';
-import { AuthContext } from './context/';
-import { ROUTES, STORAGE_NAMES } from './constants';
+import React, { useState } from "react";
+import "./styles/styles.scss";
+import { Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Main from "./pages/Main";
+import Project from "./pages/Project";
+import FeedbackForm from "./pages/FeedbackForm";
+import UserProfile from "./pages/UserProfile";
+import Menu from "./components/Menu";
+import PrivateRoute from "./PrivateRoute";
+import history from "./history";
+import { AuthContext } from "./context/";
+import { ROUTES, STORAGE_NAMES } from "./constants";
 
 const App = () => {
   const sessionStorageUser = sessionStorage.getItem(STORAGE_NAMES.USER);
   const [authTokens, setAuthTokens] = useState(
-      sessionStorageUser === 'undefined' || sessionStorageUser === null ? '' : sessionStorageUser
+    sessionStorageUser === "undefined" || sessionStorageUser === null ? "" : sessionStorageUser
   );
 
-  const setTokens = (data) => {
+  const setTokens = data => {
     sessionStorage.setItem(STORAGE_NAMES.USER, JSON.stringify(data));
     setAuthTokens(data);
   };
 
   return (
-      <AuthContext.Provider value={{authTokens, setAuthTokens: setTokens}}>
-        <Router history={history}>
-              <Menu/>
-              <Route exact path={ROUTES.HOME} component={Home}/>
-              <PrivateRoute path={ROUTES.MAIN} component={Main}/>
-              <PrivateRoute path={ROUTES.PROJECT} component={Project}/>
-              <PrivateRoute path={`${ROUTES.USER_PROFILE}/:id`} component={UserProfile}/>
-              <PrivateRoute path={ROUTES.FEEDBACKFORM} component={FeedbackForm}/>
-        </Router>
-      </AuthContext.Provider>
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+      <Router history={history}>
+        <Menu />
+        <Route exact path={ROUTES.HOME} component={Home} />
+        <PrivateRoute path={ROUTES.MAIN} component={Main} />
+        <PrivateRoute path={`${ROUTES.PROJECT}/:id`} component={Project} />
+        <PrivateRoute path={`${ROUTES.USER_PROFILE}/:id`} component={UserProfile} />
+        <PrivateRoute path={ROUTES.FEEDBACKFORM} component={FeedbackForm} />
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,10 +1,32 @@
 import React, { useEffect, useState, useContext } from "react";
-import endpoints from "../constants/endpoints";
+import { ENDPOINTS } from "../constants/endpoints";
+import { getUserId } from "../helpers/storages";
+import ProjectTile from "../components/ProjectTile";
 
 const Main = () => {
   const [projects, setProjects] = useState([]);
 
-  return <div>Main Page (after login)</div>;
+  const userId = getUserId();
+
+  useEffect(() => {
+    fetch(ENDPOINTS.PROJECTS + userId).then(response => {
+      if (!response.ok) {
+        throw new Error("Not 200 response");
+      } else {
+        response.json().then(data => {
+          setProjects(data);
+        });
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      {projects.map(project => (
+        <ProjectTile title={project.title} />
+      ))}
+    </div>
+  );
 };
 
 export default Main;
